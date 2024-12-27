2248a6d62836fddb011bca5de34561a75effaa62
Parshav Shah <parshavshah1995@gmail.com>
2024-12-27 18:35:55 +0530

    added signup code and react query things

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,46 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import { useQuery } from "@tanstack/react-query";
 
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { axiosInstance } from "./lib/axios";
+import toast from "react-hot-toast";
 
 export default function App() {
+  const { data: authUser, isLoading } = useQuery({
+    queryKey: "authUser",
+    queryFn: async () => {
+      try {
+        const res = await axiosInstance.get("/auth/me");
+        return res.data;
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          return null;
+        }
+        toast.error(err.response.data.message || "Something went wrong");
+      }
+    },
+  });
+
+  if (isLoading) return null;
+
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/signup" element={<SignupPage/>} />
-        <Route path="/login" element={<LoginPage/>} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignupPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
       </Routes>
     </Layout>
   );
---
+++ frontend/src/components/auth/SingupForm.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { axiosInstance } from "../../lib/axios";
+import { toast } from "react-hot-toast";
+
+const SingupForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const queryClient = useQueryClient();
+
+  const { mutate: signupMutation, isLoading } = useMutation({
+    mutationFn: async (data) => {
+      const res = await axiosInstance.post("/auth/signup", data);
+      return res.data;
+    },
+    onSuccess: async () => {
+      toast.success("Account created successfully");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+    onError: async (err) => {
+      toast.error(err.response.data.message || "Something went wrong ");
+    },
+  });
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log(name, email, username, password);
+    signupMutation({ name, email, username, password });
+  };
+
+  return (
+    <form onSubmit={handleSignUp} className="flex flex-col gap-4">
+      <input
+        type="text"
+        placeholder="Full name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="input input-bordered w-full"
+        required
+      />
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className="input input-bordered w-full"
+        required
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="input input-bordered w-full"
+        required
+      />
+      <input
+        type="password"
+        placeholder="Password (6+ characters)"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="input input-bordered w-full"
+        required
+      />
+
+      <button
+        type="submit"
+        disabled={isLoading}
+        className="btn btn-primary w-full text-white"
+      >
+        {isLoading ? (
+          <Loader className="size-5 animate-spin" />
+        ) : (
+          "Agree & Join"
+        )}
+      </button>
+    </form>
+  );
+};
+
+export default SingupForm;
